1aa952a071b46d301db0e37be2ccfc69c99ffcd4
Michal Deak <miso.deak1990@gmail.com>
2019-02-15 16:10:58 +0100

    Added PausePopup and IngameScreen.

---
+++ Assets/Scripts/UI/Screens/IngameScreen/IngameScreenBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameScreenBehaviour : MonoBehaviour
+{
+    public Button InventoryButton;
+    public Button QuestButton;
+    public Button AchievementButton;
+    public Button PauseButton;
+    public Text InfoLabel;
+    public Slider HealthBarSlider;
+}
